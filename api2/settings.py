020342a225f7a5df381d7d03b93690e6d8e6345f
alby <albertagoya@gmail.com>
2019-07-04 11:42:16 +0300

    Addfred permission rest amework

--- api2/settings.py
+++ api2/settings.py
@@ -46,7 +46,16 @@ INSTALLED_APPS = [
     'blog',
     'mpesa',
     'lnmp'
+    
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
