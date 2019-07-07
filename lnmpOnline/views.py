c1e418e4191f0fa7b0cd28ef0de0d15c42ac06ed
alby <albertagoya@gmail.com>
2019-07-07 09:57:38 +0300

    permission_classes = [AllowAny]

--- lnmpOnline/views.py
+++ lnmpOnline/views.py
@@ -36,8 +36,11 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import AllowAny
+
 
 class LNMPList(APIView):
+    permission_classes = [AllowAny]
     """
     List all lnmp or create another lnmp
     """
