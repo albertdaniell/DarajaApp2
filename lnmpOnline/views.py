2b28034858e1223c84d290f4a5399ae840449178
alby <albertagoya@gmail.com>
2019-07-07 10:54:48 +0300

    from datetime import datetime

--- lnmpOnline/views.py
+++ lnmpOnline/views.py
@@ -38,6 +38,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from datetime import datetime
+
 
 
 class LNMPList(CreateAPIView):
