374cf437223c7bbf0ec15535c05ffbcbae9f8873
alby <albertagoya@gmail.com>
2019-07-07 06:23:22 +0300

    Altering the lnmp

--- lnmp/views.py
+++ lnmp/views.py
@@ -1,66 +1,126 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
-
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from lnmp.models import LipaNaMpesa
 from lnmp.serializers import LNMPSerializer
 from datetime import datetime
 
-class LNMPApiView(CreateAPIView):
-    queryset = LipaNaMpesa.objects.all()
-    serializer_class = LNMPSerializer
-    permission_classes = [AllowAny]
 
-    def create(self, request):
-        print("This is the data received...")
-        print(request.data, "This is the request.data")
-        data=request.data
+# BEGINING OF SAMPLE
 
-        testcode=(data['Body']['stkCallback']['ResultCode'])
-        MerchantRequestID=(data['Body']['stkCallback']['MerchantRequestID'])
-        CheckoutRequestID=(data['Body']['stkCallback']['CheckoutRequestID'])
-        ResultCode = (data['Body']['stkCallback']['ResultCode'])
-        ResultDescription = (data['Body']['stkCallback']['ResultDesc'])
-        Amount=(data['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value'])
-        mpesa_receipt_number=(data['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value'])
-        MpesaReceiptNumber=''
-        TransactionDate=(data['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value'])
-        # convert date
-        TransactionDate=str(TransactionDate)
-        TransactionDate=datetime.strptime(TransactionDate,("%Y%m%d%H%M%S"))
+@csrf_exempt
+def lnmp_list(request):
+    """"
+    List  all mpesa calls
+    """
+    if request.method =="GET":
+        mpesas=LipaNaMpesa.objects.all()
+        serializer=LNMPSerializer(mpesas,many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method =="POST":
+        data = JSONParser().parse(request)
+        serializer=LNMPSerializer(data=data)
+        if serializer.is_valid():
+            print("This is the data received...")
+            print(request.data, "This is the request.data")
+            data=request.data
 
-        PhoneNumber=(data['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value'])
+            testcode=(data['Body']['stkCallback']['ResultCode'])
+            MerchantRequestID=(data['Body']['stkCallback']['MerchantRequestID'])
+            CheckoutRequestID=(data['Body']['stkCallback']['CheckoutRequestID'])
+            ResultCode = (data['Body']['stkCallback']['ResultCode'])
+            ResultDescription = (data['Body']['stkCallback']['ResultDesc'])
+            Amount=(data['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value'])
+            mpesa_receipt_number=(data['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value'])
+            MpesaReceiptNumber=''
+            TransactionDate=(data['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value'])
+            # convert date
+            TransactionDate=str(TransactionDate)
+            TransactionDate=datetime.strptime(TransactionDate,("%Y%m%d%H%M%S"))
 
-        # print(TransactionDate)
+            PhoneNumber=(data['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value'])
 
-        mpesa_data_callbk={
-            "testcode":testcode,
-            "MerchantRequestID":MerchantRequestID,
-            "CheckoutRequestID":CheckoutRequestID,
-            "ResultCode":ResultCode,
-            "Amount":Amount,
-            "mpesa_receipt_number":mpesa_receipt_number,
-            "TransactionData":TransactionDate,
-            "PhoneNumber":PhoneNumber
-        }
+            # print(TransactionDate)
 
-        print(TransactionDate)
+            mpesa_data_callbk={
+                "testcode":testcode,
+                "MerchantRequestID":MerchantRequestID,
+                "CheckoutRequestID":CheckoutRequestID,
+                "ResultCode":ResultCode,
+                "Amount":Amount,
+                "mpesa_receipt_number":mpesa_receipt_number,
+                "TransactionData":TransactionDate,
+                "PhoneNumber":PhoneNumber
+            }
 
-        from lnmp.models import LipaNaMpesa
-        model=LipaNaMpesa.objects.create(
-            checkoutRequestID=CheckoutRequestID,
-            merchantRequestID=MerchantRequestID,
-            resultCode=ResultCode,
-            resultDescription=ResultDescription,
-            transactionDate=TransactionDate,
-            phoneNumber=PhoneNumber,
-            amount=Amount
+            print(TransactionDate)
+            # serializer.save()
+            print(data)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse (serializer.errors, status=400)
 
 
-        )
-        model.save()
+# END OF EXAMPLE
+
+# ORIGINAL
+
+
+
+# class LNMPApiView(CreateAPIView):
+#     queryset = LipaNaMpesa.objects.all()
+#     serializer_class = LNMPSerializer
+#     permission_classes = [AllowAny]
+
+#     def create(self, request):
+#         print("This is the data received...")
+#         print(request.data, "This is the request.data")
+#         data=request.data
+
+#         testcode=(data['Body']['stkCallback']['ResultCode'])
+#         MerchantRequestID=(data['Body']['stkCallback']['MerchantRequestID'])
+#         CheckoutRequestID=(data['Body']['stkCallback']['CheckoutRequestID'])
+#         ResultCode = (data['Body']['stkCallback']['ResultCode'])
+#         ResultDescription = (data['Body']['stkCallback']['ResultDesc'])
+#         Amount=(data['Body']['stkCallback']['CallbackMetadata']['Item'][0]['Value'])
+#         mpesa_receipt_number=(data['Body']['stkCallback']['CallbackMetadata']['Item'][1]['Value'])
+#         MpesaReceiptNumber=''
+#         TransactionDate=(data['Body']['stkCallback']['CallbackMetadata']['Item'][3]['Value'])
+#         # convert date
+#         TransactionDate=str(TransactionDate)
+#         TransactionDate=datetime.strptime(TransactionDate,("%Y%m%d%H%M%S"))
+
+#         PhoneNumber=(data['Body']['stkCallback']['CallbackMetadata']['Item'][4]['Value'])
+
+#         # print(TransactionDate)
+
+#         mpesa_data_callbk={
+#             "testcode":testcode,
+#             "MerchantRequestID":MerchantRequestID,
+#             "CheckoutRequestID":CheckoutRequestID,
+#             "ResultCode":ResultCode,
+#             "Amount":Amount,
+#             "mpesa_receipt_number":mpesa_receipt_number,
+#             "TransactionData":TransactionDate,
+#             "PhoneNumber":PhoneNumber
+#         }
+
+#         print(TransactionDate)
+
+#         from lnmp.models import LipaNaMpesa
+#         model=LipaNaMpesa.objects.create(
+#             checkoutRequestID=CheckoutRequestID,
+#             merchantRequestID=MerchantRequestID,
+#             resultCode=ResultCode,
+#             resultDescription=ResultDescription,
+#             transactionDate=TransactionDate,
+#             phoneNumber=PhoneNumber,
+#             amount=Amount
+
+
+#         )
+#         model.save()
 
