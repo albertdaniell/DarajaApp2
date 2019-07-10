936477185f8fc21831908398b01c0cfaa4e807d7
alby <albertagoya@gmail.com>
2019-07-10 14:57:38 +0300

    return JsonResponse(serializer.data, status=201)

--- mpesa/views.py
+++ mpesa/views.py
@@ -21,7 +21,6 @@ def mpesa_list(request):
         data = JSONParser().parse(request)
         serializer=MpesaSerializer(data=data)
         if serializer.is_valid():
-            return JsonResponse(serializer.data, status=201)
             phone=data['phone_number']
             amount=data['amount']
             payBill=data['payBill']
@@ -31,6 +30,6 @@ def mpesa_list(request):
             serializer.save()
             print("The data is :",data)
             print(request)
-            
+            return JsonResponse(serializer.data, status=201)
         return JsonResponse (serializer.errors, status=400)
 
